5c2fad82a87b9b7f3cbd59ed89240e7e0383351f
Juan Hoyos <juanse.jsh@gmail.com>
2020-09-11 00:23:28 -0400

    Netflix clone front end

---
+++ netflix-clone/src/request.js
@@ -0,0 +1,14 @@
+  const API_KEY = "a43bf3b6aada409c0822f54dec140f9b";
+
+  const requests={
+    fetchTrending:`/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals:`/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated:`/movie/top_rated/?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies:`/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies:`/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies:`/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies:`/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentationMovies:`/discover/movie?api_key=${API_KEY}&with_genres=99`,
+  }
+
+  export default requests;
